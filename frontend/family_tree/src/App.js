845e29a3badd522ec7dd0100ea721ac74d450b1b
Dev Goel <goel13jan@yahoo.co.in>
2021-09-08 19:53:20 +0530

    Created Search Field Component

--- frontend/family_tree/src/App.js
+++ frontend/family_tree/src/App.js
@@ -1,9 +1,13 @@
+import React from "react";
 import './App.css';
 import Help from './Components/Help.js';
+import SearchBar from "./Components/SearchBar";
+import StudentData from "./Data.json";
 
 function App() {
   return (
     <div className="App">
+      <SearchBar placeholder="Enter the Name or Roll Number..." data={StudentData} />
       <div className="help">
         <Help />
       </div>
