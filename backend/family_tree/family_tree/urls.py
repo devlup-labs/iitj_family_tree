20e3c2a2cb30485c63a3643fcd45c9f9756b04f7
akshatjain1004 <jain.53@iitj.ac.in>
2021-09-13 18:43:36 +0530

    made name changes in the file

--- backend/family_tree/family_tree/urls.py
+++ backend/family_tree/family_tree/urls.py
@@ -20,5 +20,5 @@ from graphene_django.views import GraphQLView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("graphql", GraphQLView.as_view(graphiql=True)),
-    path('students/', include('student_model.urls')),
+    path('students/', include('student.urls')),
 ]
