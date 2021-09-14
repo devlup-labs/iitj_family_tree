f6b3a4a78620de07429bf775dde9d7e01e1b25f8
Ronak <ronakmohata@gmail.com>
2021-09-14 16:23:33 +0530

    created a schema

---
+++ backend/family_tree/student/schema.py
@@ -0,0 +1,13 @@
+import graphene
+from graphene_django import DjangoObjectType
+from student.models import Student
+
+class StudentType(DjangoObjectType):
+    class Meta:
+        model= Student
+        fields= ("id","name","branch","year","picture","homeTown","extraCurriculars","socialMedia", "linkedIn","email","parentId","roll_no")
+
+class Query(graphene.ObjectType):
+    all_students=graphene.List(StudentType)
+
+schema=graphene.Schema(query=Query)
