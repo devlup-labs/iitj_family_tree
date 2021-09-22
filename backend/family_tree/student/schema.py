2c0ee8709268815e50a44780e9418238b773720b
Ronak <ronakmohata@gmail.com>
2021-09-22 16:18:25 +0530

    added filter fields

--- backend/family_tree/student/schema.py
+++ backend/family_tree/student/schema.py
@@ -6,8 +6,9 @@ class StudentType(DjangoObjectType):
     class Meta:
         model= Student
         fields= ("id","name","branch","year","picture","homeTown","extraCurriculars","socialMedia", "linkedIn","email","parentId","roll_no")
+        filter_fields=["id","name","branch","year","email","parentId","roll_no"]
 
 class Query(graphene.ObjectType):
-    all_students=graphene.List(StudentType)
+    students=graphene.List(StudentType)
 
 schema=graphene.Schema(query=Query)
