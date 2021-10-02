702c7a4cd5cb6f76735e00fce43fd68ba94f498d
akshatjain1004 <jain.53@iitj.ac.in>
2021-10-02 13:19:09 +0530

    added social media class

--- backend/family_tree/student/models.py
+++ backend/family_tree/student/models.py
@@ -7,13 +7,15 @@ class Student(models.Model):
     year= models.CharField(max_length=4)
     picture= models.URLField(max_length=200)
     homeTown= models.CharField(max_length=200)
-    extraCurriculars= models.CharField()
-    socialMedia= models.URLField()
+    extraCurriculars= models.CharField(max_length=2000)
+    socialMedia= models.ForeignKey('SocialMedia')
     linkedIn= models.URLField(max_length=200)
     email= models.EmailField(max_length=254)
-    parentId= models.CharField()
+    parentId= models.CharField(max_length=9)
 
-
+class SocialMedia(models.Model):
+		facebook = models.URLField(max_length=255)
+		instagram = models.URLField(max_length=255)
     
 
 
