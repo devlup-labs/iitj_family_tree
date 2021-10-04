93b5c4df9192c8def2f30321a63c58920fea13da
akshatjain1004 <jain.53@iitj.ac.in>
2021-10-04 19:57:23 +0530

    Added Social Media field

--- backend/family_tree/student/models.py
+++ backend/family_tree/student/models.py
@@ -7,20 +7,12 @@ class Student(models.Model):
     year= models.CharField(max_length=4)
     picture= models.URLField(max_length=200)
     homeTown= models.CharField(max_length=200)
-<<<<<<< HEAD
-    extraCurriculars= models.CharField(max_length=2000)
-    socialMedia= models.ForeignKey('SocialMedia')
-    linkedIn= models.URLField(max_length=200)
-    email= models.EmailField(max_length=254)
-    parentId= models.CharField(max_length=9)
-=======
     extraCurriculars= models.CharField(max_length=400)
-    socialMedia= models.URLField()
+    socialMedia= models.ForeignKey('SocialMedia')
     linkedIn= models.URLField(max_length=200)
     email= models.EmailField(max_length=254)
     parentId= models.CharField(max_length=400)
 
->>>>>>> f3c5d575314c25efa18736f1f5355e7c052ec9a5
 
 class SocialMedia(models.Model):
 		facebook = models.URLField(max_length=255)
