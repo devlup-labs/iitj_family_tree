a8374cf64b3d78b9caf617ef0daac69009aa829c
akshatjain1004 <jain.53@iitj.ac.in>
2021-10-30 21:56:12 +0530

    corrected the PR

--- backend/family_tree/student/schema.py
+++ backend/family_tree/student/schema.py
@@ -10,17 +10,20 @@ class StudentType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
     students=graphene.List(StudentType)
+    path= graphene.List(StudentType, year=graphene.Decimal())
+    batch= graphene.List(StudentType, temp=graphene.String(), currentYear=graphene.Decimal())
+    def dumyFunc(year):
+        path=[]
+        for i in range(1,year-2011):
+            path.append(i)
+        return path
+    def resolveBatch(currentYear,temp):
+        batch=[]
+        while(Student.objects.filter(roll_no=temp)[0].year== currentYear):
+            batch.append(dumyFunc(currentYear))
+            temp= Student.objects.filter(roll_no=temp)[0]+1
+        return batch
 
 schema=graphene.Schema(query=Query)
 
-def dumyFunc(year):
-    path=[]
-    for i in range(1,year-2011):
-        path.append(i)
-    return path
-def resolveBatch(currentYear,temp):
-    batch=[]
-    while(Student.objects.filter(roll_no=temp)[0].year== currentYear):
-        batch.append(dumyFunc(currentYear))
-        temp= Student.objects.filter(roll_no=temp)[0]+1
-    return batch
+    
