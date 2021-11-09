83073409f10075e4b09d07972e07c76e780748a7
Ronak <ronakmohata@gmail.com>
2021-11-09 22:15:29 +0530

    changed filter to "get"

--- backend/family_tree/student/schema.py
+++ backend/family_tree/student/schema.py
@@ -12,17 +12,18 @@ class StudentType(DjangoObjectType):
 class Query(graphene.ObjectType):
     students=graphene.List(StudentType)
     student_path= graphene.List(StudentType, roll=graphene.String())
+
     def resolve_students(root,info):
         return Student.objects.all()
+
     def resolve_student_path(root, info, roll):
         pathObjects=[]
-        while(Student.objects.filter(roll_no=roll)[0].parentId!="root"):
-            pathObjects.append(Student.objects.filter(roll_no=roll)[0])
-            roll= Student.objects.filter(roll_no=roll)[0].parentId
-        pathObjects.append(Student.objects.filter(roll_no=roll)[0])
+        while(Student.objects.get(roll_no=roll).parentId!="root"):
+            k=Student.objects.get(roll_no=roll)
+            pathObjects.append(k)
+            roll= k.parentId
+        pathObjects.append(Student.objects.get(roll_no=roll))
         return pathObjects
-    
 
-        
 
 schema=graphene.Schema(query=Query)
