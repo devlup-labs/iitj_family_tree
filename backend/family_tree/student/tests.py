78ccbe10311f1f31c9b690a0310bf444e599cc92
Ronak <ronakmohata@gmail.com>
2021-12-14 21:14:46 +0530

    added test for student_sibling resolver

--- backend/family_tree/student/tests.py
+++ backend/family_tree/student/tests.py
@@ -8,8 +8,10 @@ class testCasesForStudents(GraphQLTestCase):
     GRAPHQL_URL = 'http://localhost/graphql'
     def setUp(self):
       super().setUp()
-      self.student1 = mixer.blend(Student, name='student1')
-      self.student2 = mixer.blend(Student, name='student2')
+      self.student1 = mixer.blend(Student, name='student1', roll_no='1', parentId='root')
+      self.student2 = mixer.blend(Student, name='student2', roll_no='2', parentId='1')
+      self.student3 = mixer.blend(Student, name='student3', roll_no='3', parentId='1')
+      self.student4 = mixer.blend(Student, name='student4', roll_no='4', parentId='1') 
     def test_student_query(self):
         response = self.query('''
             query {
@@ -23,3 +25,16 @@ class testCasesForStudents(GraphQLTestCase):
         self.assertResponseNoErrors(response)
         self.assertDictEqual(content['data']['students'][0], {'id': str(self.student1.id), 'name': self.student1.name})
         self.assertDictEqual(content['data']['students'][1], {'id': str(self.student2.id), 'name': self.student2.name})
+    def test_student_sibling(self):
+        response = self.query('''
+            query {
+                studentSibling(roll: "2") {
+                    id
+                    name
+                  }
+                }
+              ''')
+        content= json.loads(response.content)
+        self.assertResponseNoErrors(response)
+        self.assertDictEqual(content['data']['studentSibling'][0], {'id': str(self.student3.id), 'name': self.student3.name})
+        self.assertDictEqual(content['data']['studentSibling'][1], {'id': str(self.student4.id), 'name': self.student4.name})
