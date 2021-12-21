00484560d3f3b0fbd4289d4847a8945fc7242375
akshatjain1004 <jain.53@iitj.ac.in>
2021-12-21 16:58:00 +0530

    creates test cases for fetch batch resolver

--- backend/family_tree/student/tests.py
+++ backend/family_tree/student/tests.py
@@ -1,3 +1,4 @@
+from django.http import response
 from django.test import TestCase
 import json
 from graphene_django.utils.testing import GraphQLTestCase
@@ -8,10 +9,10 @@ class testCasesForStudents(GraphQLTestCase):
     GRAPHQL_URL = 'http://localhost/graphql'
     def setUp(self):
       super().setUp()
-      self.student1 = mixer.blend(Student, name='student1', roll_no='1', parentId='root')
-      self.student2 = mixer.blend(Student, name='student2', roll_no='2', parentId='1')
-      self.student3 = mixer.blend(Student, name='student3', roll_no='3', parentId='1')
-      self.student4 = mixer.blend(Student, name='student4', roll_no='4', parentId='1') 
+      self.student1 = mixer.blend(Student, name='student1', roll_no='1', parentId='root', year=2019)
+      self.student2 = mixer.blend(Student, name='student2', roll_no='2', parentId='1', year=2020)
+      self.student3 = mixer.blend(Student, name='student3', roll_no='3', parentId='1', year=2020)
+      self.student4 = mixer.blend(Student, name='student4', roll_no='4', parentId='1', year=2020) 
     def test_student_query(self):
         response = self.query('''
             query {
@@ -38,3 +39,23 @@ class testCasesForStudents(GraphQLTestCase):
         self.assertResponseNoErrors(response)
         self.assertDictEqual(content['data']['studentSibling'][0], {'id': str(self.student3.id), 'name': self.student3.name})
         self.assertDictEqual(content['data']['studentSibling'][1], {'id': str(self.student4.id), 'name': self.student4.name})
+    
+    def test_student_batch(self):
+      response= self.query('''
+            query {
+                studentBatch(roll: "2") {
+                    id
+                    name
+                  }
+                }
+              ''')
+      content= json.loads(response.content)
+      self.assertResponseNoErrors(response)
+      self.assertDictEqual(content['data']['studentBatch'][0][0], {'id': str(self.student2.id), 'name': self.student2.name})
+      self.assertDictEqual(content['data']['studentBatch'][0][1], {'id': str(self.student1.id), 'name': self.student1.name})
+      self.assertDictEqual(content['data']['studentBatch'][1][0], {'id': str(self.student3.id), 'name': self.student3.name})
+      self.assertDictEqual(content['data']['studentBatch'][1][1], {'id': str(self.student1.id), 'name': self.student1.name})
+      self.assertDictEqual(content['data']['studentBatch'][2][0], {'id': str(self.student4.id), 'name': self.student4.name})
+      self.assertDictEqual(content['data']['studentBatch'][1][1], {'id': str(self.student1.id), 'name': self.student1.name})
+        
+        
